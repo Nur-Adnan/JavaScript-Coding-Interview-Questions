6ac345a77d035058eab05d6dbb15277c2d891b61
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-10-24 01:40:15 +0600

    sol question 1

--- Q1.js
+++ Q1.js
@@ -1,1 +1,8 @@
 // How to find duplicate elements in an array?
+const arrNumber = [1, 2, 8, 2, 9, 8];
+
+const duplicateElements = arrNumber.filter(
+  (element, index, arr) => arr.indexOf(element) !== index
+);
+
+console.log(duplicateElements); // Output: [2, 8]
