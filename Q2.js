c6c35a2ffc857082c62550ea5e83d58e3a824eda
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-10-24 11:12:10 +0600

    solved question 2

--- Q2.js
+++ Q2.js
@@ -1,1 +1,21 @@
 //  How to find max and min value in a given array in js
+
+const array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+
+// const maxValue = Math.max(...array);
+// const minValue = Math.min(...array);
+
+let maxValue = array[0];
+let minValue = array[0];
+
+for (let i = 1; i < array.length; i++) {
+  if (array[i] > maxValue) {
+    maxValue = array[i];
+  }
+  if (array[i] < minValue) {
+    minValue = array[i];
+  }
+}
+
+console.log(`Max value: ${maxValue}`);
+console.log(`Min value: ${minValue}`);
